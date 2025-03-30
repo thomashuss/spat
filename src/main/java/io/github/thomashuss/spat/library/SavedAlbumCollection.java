5e063a1a2a2b6c494576c29327dfb04d12068a22
Thomas Huss <45053302+thomashuss@users.noreply.github.com>
2025-03-29 23:19:18 -0400

    Add SavedTrackCollection and SavedAlbumCollection

---
+++ src/main/java/io/github/thomashuss/spat/library/SavedAlbumCollection.java
@@ -0,0 +1,15 @@
+package io.github.thomashuss.spat.library;
+
+public class SavedAlbumCollection
+    extends SavedResourceCollection<Album>
+{
+    SavedAlbumCollection()
+    {
+        super();
+    }
+
+    SavedAlbumCollection(String name)
+    {
+        super(name);
+    }
+}
---
+++ src/main/java/io/github/thomashuss/spat/library/SavedTrackCollection.java
@@ -0,0 +1,15 @@
+package io.github.thomashuss.spat.library;
+
+public class SavedTrackCollection
+    extends SavedResourceCollection<Track>
+{
+    SavedTrackCollection()
+    {
+        super();
+    }
+
+    SavedTrackCollection(String name)
+    {
+        super(name);
+    }
+}
